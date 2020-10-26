4278569da761b699c97856e20efaef9e4cf6d61f
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-26 13:29:27 -0600

    added code to get product photo

--- controllers/product.js
+++ controllers/product.js
@@ -16,6 +16,14 @@ exports.productById = (req, res, next, id) => {
   });
 };
 
+exports.photo = (req, res, next) => {
+  if(req.product.photo.data) {
+    res.set("Content-Type", req.product.photo.contentType);
+    return res.send(req.product.photo.data)
+  }
+  next();
+}
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
