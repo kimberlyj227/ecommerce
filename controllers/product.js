e4a3f485f92a3fee4623859cf241235c50c63ad8
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-26 13:09:24 -0600

    add related products route

--- controllers/product.js
+++ controllers/product.js
@@ -141,7 +141,27 @@ exports.list = (req, res) => {
           message: "Products not found"
         })
       }
-      res.send(products)
-    })
+      res.json(products)
+    });
   
+}
+// *** RELATED PRODUCTS ***//
+// find products based on the req product category
+// products that have same category will be returned
+// *** //
+
+exports.listRelated = (req, res) => {
+  let limit = req.query.limit ? parseInt(req.query.limit) : 10;
+
+  Product.find({ _id: {$ne: req.product}, category: req.product.category })
+    .limit(limit)
+    .populate("category", "_id name")
+    .exec((err, products) => {
+      if(err) {
+        return res.status(400).json({
+          message: "Products not found"
+        })
+      }
+      res.json(products)
+    })
 }
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated } = require("../controllers/product");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -10,7 +10,8 @@ router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
 router.delete("/product/:productId/:userId", requireSignin, isAdmin, isAuth, remove);
 router.put("/product/:productId/:userId", requireSignin, isAdmin, isAuth, update);
 
-router.get("/products", list)
+router.get("/products", list);
+router.get("/products/related/:productId", listRelated)
 
 router.param("userId", userById);
 router.param("productId", productById);
