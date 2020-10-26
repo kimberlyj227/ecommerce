282e2a4a41422a7cb740da546d4c968ad1dc1ded
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-26 10:18:26 -0600

    created category read route

--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create, categoryById, read } = require("../controllers/category");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-
+router.get("/category/:categoryId", read)
 router.post("/category/create/:userId", requireSignin, isAdmin, isAuth, create);
 
+
 router.param("userId", userById);
+router.param("categoryId", categoryById);
 
 module.exports = router;
