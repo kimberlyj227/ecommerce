d6d7ad93ef2150e2b1ff938a57dd977ca3b2ffd2
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-25 23:53:13 -0600

    added sign up validator, sign in and sign out routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const expresJwt = require("express-jwt"); //auth check
 const { errorHandler } = require("../helpers/dbErrorHandler")
 
 exports.signup = (req, res) => {
@@ -17,3 +19,33 @@ exports.signup = (req, res) => {
     });
   })
 };
+
+exports.signin = (req, res) => {
+  // find user based on email
+  const {email, password } = req.body
+  User.findOne({email}, (err, user) => {
+    if(err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please sign up"
+      })
+    }
+    // if user is found, make sure email and password match
+    // create auth method in user model
+    if(!user.authenticate(password)) {
+      return res.status(401).json({ error: "Email and password don't match"})
+    }
+
+    // generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id}, process.env.JWT_SECRET)
+    // persist the token at "t" in cookie with exp date
+    res.cookie("t", token, {expire: new Date() + 9999});
+    //return res with user and token to front end
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role }});
+  })
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  res.json({ message: "Signout successful"});
+}
