ecf549c73be82fbf27158b695d23eb8a5a20c31b
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-26 15:06:42 -0600

    created menu component

--- ecommerce-front/src/Routes.js
+++ ecommerce-front/src/Routes.js
@@ -3,11 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
+import Menu from "./core/Menu";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 
 const Routes = () => {
   return (
     <Router>
+      <Menu />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
--- ecommerce-front/src/index.js
+++ ecommerce-front/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
+    <Routes />,
   document.getElementById('root')
 );
 
