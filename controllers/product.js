735ba36cd763fa6a1b7754fd0784867a76a36dd7
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-26 09:59:31 -0600

    created product delete route

--- controllers/product.js
+++ controllers/product.js
@@ -61,3 +61,17 @@ exports.read = (req, res) => {
   req.product.photo = undefined;
   return res.json(req.product);
 }
+
+exports.remove = (req, res) => {
+  let product = req.product
+  product.remove((err, deletedProduct) => {
+    if(err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json({
+      message: "Product deleted successfully"
+    })
+  })
+}
