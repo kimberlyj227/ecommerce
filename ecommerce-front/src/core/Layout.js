1bcc196539fe3d31fd8a50d893c6e066cde7c9b1
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-27 09:38:23 -0600

    added styles to jumbotron

--- ecommerce-front/src/core/Layout.js
+++ ecommerce-front/src/core/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Jumbotron } from "react-bootstrap";
-import Menu from "./Menu"
+import Menu from "./Menu";
+import "../styles.css";
 
 
 const Layout = ({ title = "Title", description ="Description", className, children }) => {
