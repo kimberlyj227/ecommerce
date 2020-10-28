71856584ecded82be61bd3a3329ad79a0185d8f8
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-28 16:35:31 -0600

    update sold and inventory quanties based on how many items user buys

--- ecommerce/routes/order.js
+++ ecommerce/routes/order.js
@@ -4,9 +4,17 @@ const router = express.Router();
 const { userById, addOrderToHistory  } = require("../controllers/user");
 const { requireSignin, isAuth } = require("../controllers/auth");
 const { create} = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
 
-router.post("/order/create/:userId", requireSignin, isAuth, addOrderToHistory, create);
+router.post(
+  "/order/create/:userId", 
+  requireSignin, 
+  isAuth, 
+  addOrderToHistory, 
+  decreaseQuantity, 
+  create
+);
 
 
 router.param("userId", userById)
