aeaf4cf4ea3c10907c73673cfa283662e66036a9
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-28 13:30:34 -0600

    added front end route for braintree token and added payment dashboard for checkout

--- ecommerce-front/src/core/Checkout.js
+++ ecommerce-front/src/core/Checkout.js
@@ -2,12 +2,42 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import Search from "./Search";
 import ProductCard from "./Card";
-import { getProducts } from "./apiCore";
+import { getProducts, getBraintreeClientToken } from "./apiCore";
 import { Row, Col, Button } from "react-bootstrap";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import DropIn from "braintree-web-drop-in-react";
 
 const Checkout =({products}) => {
+
+  const [data, setData] = useState({
+    success: false,
+    clientToken: null,
+    error: "",
+    instance: {},
+    address: ""
+  });
+
+  const userId = isAuthenticated() && isAuthenticated().user._id;
+  const token = isAuthenticated() && isAuthenticated().token;
+
+  useEffect(() => {
+    getToken(userId, token)
+  }, [])
+
+
+  const getToken = (userId, token) => {
+    getBraintreeClientToken(userId, token).then(res => {
+      if(res.error) {
+        setData({...data, error: res.error})
+      } else {
+        setData({
+          ...data,
+          clientToken: res.clientToken
+        })
+      }
+    })
+  }
 
   const getTotal = () => {
     return products.reduce((currentValue, nextValue) => {
@@ -18,9 +48,9 @@ const Checkout =({products}) => {
   const showCheckout = () => {
    
     return isAuthenticated() ? (
-        <Button variant="success">
-          Checkout
-        </Button>
+        <div >
+          {showDropIn()}
+        </div>
       ) : (
         <Link to="/signin">
           <Button variant="info">
@@ -29,6 +59,26 @@ const Checkout =({products}) => {
         </Link>
       )
     
+  }
+
+  const showDropIn = () => {
+    return (
+      <div>
+        {data.clientToken !== null && products.length > 0 ? (
+          <div>
+            <DropIn options={{
+              authorization: data.clientToken,
+
+            }} onInstance={instance => (data.instance = instance)}
+            />
+            <Button variant="success">
+              Checkout
+            </Button>
+          </div>
+
+        ) : null}
+      </div>
+    )
   }
 
 
