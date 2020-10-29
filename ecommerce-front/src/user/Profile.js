4b2aa132dd58269b1ed3ccbab28164a94e3a618c
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-29 10:57:36 -0600

    added ability for user to update name, email, password

--- ecommerce-front/src/user/Profile.js
+++ ecommerce-front/src/user/Profile.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
-import { Card, ListGroup, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Form, Button, Col, Row, Alert } from "react-bootstrap";
+import { Link, Redirect } from "react-router-dom";
 import { read, update, updateUser } from "./apiUser";
 
 const Profile = ({match}) => {
   const { token } = isAuthenticated();
+  const userId = match.params.userId;
   
   const [userInfo, setUserInfo] = useState({
     name: "",
@@ -19,8 +20,8 @@ const Profile = ({match}) => {
   const { name, email, password } = userInfo;
 
   useEffect(() => {
-    init(match.params.userId);
-  });
+    init(userId);
+  }, []);
 
   const init = (userId) => {
     read(userId, token).then(data => {
@@ -33,6 +34,95 @@ const Profile = ({match}) => {
         })
       }
     });
+  };
+
+  const clickSubmit = (e) => {
+    e.preventDefault();
+    update(userId, token, {name, email, password}).then(data => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        updateUser(data, () => {
+          setUserInfo({...userInfo, name: data.name, email: data.email });
+          setSuccess(true);
+        })
+      }
+    })
+  }
+
+  const redirectUser = success => {
+    if(success) {
+      return <Redirect to="/cart" />
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setError(false);
+    setUserInfo({...userInfo, [name]: value})
+  }
+
+  const showError = () => (
+    <Alert variant= "danger" style={{display: error ? "" : "none"}}>
+      {error}
+    </Alert>
+  )
+ 
+  const showSuccess = () => (
+   <Alert variant="success" style={{display: success ? "" : "none"}}>
+     <h5>{`Profile update successful!`}</h5>
+   </Alert>
+  )
+
+  const profileUpdate = (name, email, password) => {
+    return (
+
+    <Form className="mb-3" onSubmit={clickSubmit}>
+      
+        <Form.Group >
+          <Form.Label className="text-muted">Name</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Product Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            autoFocus
+          />
+        </Form.Group>
+
+        <Form.Group  >
+          <Form.Label className="text-muted">Email</Form.Label>
+          <Form.Control 
+            type="text" 
+            name="email"
+            value={email}
+            autoFocus
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group >
+          <Form.Label className="text-muted">Password</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            autoFocus
+             />
+        </Form.Group>
+
+      <Form.Row>
+        <Button as={Col} variant="outline-info" type="submit" onClick={clickSubmit}>
+          Save Changes
+        </Button> 
+
+      </Form.Row>
+  
+    </Form>
+    )
   }
   
 
@@ -43,6 +133,8 @@ const Profile = ({match}) => {
       className="container-fluid"
     >
       <h2>Profile Update</h2>
+      {profileUpdate(name, email, password)}
+      {redirectUser(success)}
 
       
 
