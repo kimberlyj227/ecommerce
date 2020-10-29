8071cc3ba8fb1112ba5e59958d74260e24235bd2
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-29 11:29:26 -0600

    backend routes for user purchase history

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { userById, read, update } = require("../controllers/user");
+const { userById, read, update, purchaseHistory } = require("../controllers/user");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 
 router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
@@ -12,6 +12,7 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
 
 router.get("/user/:userId", requireSignin, isAuth, read )
 router.put("/user/:userId", requireSignin, isAuth, update )
+router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory )
 
 
 router.param("userId", userById);
