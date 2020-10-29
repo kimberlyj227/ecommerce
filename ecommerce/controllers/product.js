15f2c84af1d3183ac5b306de5d183a92d4f1615e
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-10-29 16:01:56 -0600

    updated product update route

--- ecommerce/controllers/product.js
+++ ecommerce/controllers/product.js
@@ -97,12 +97,12 @@ exports.update = (req, res) => {
     }
 
     //check for all fields
-    const { name, description, price, category, quantity, shipping } = fields;
-    if (!name || !description || !price || !category || !shipping || !quantity) {
-      return res.status(400).json({
-        error: "All fields are required"
-      })
-    }
+    // const { name, description, price, category, quantity, shipping } = fields;
+    // if (!name || !description || !price || !category || !shipping || !quantity) {
+    //   return res.status(400).json({
+    //     error: "All fields are required"
+    //   })
+    // }
 
     let product = req.product;
     product = _.extend(product, fields);
